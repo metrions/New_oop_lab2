63a11278a8347efa317cb12b2e9c7f7f5c62d0de
PANARIN <panarin@PANARIN.panarin>
2025-02-26 19:09:42 +0700

    first commit

---
+++ src/main/java/com/example/ooplab1/OopLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.ooplab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopLab1Application.class, args);
+    }
+
+}
